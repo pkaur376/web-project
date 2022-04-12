c8859a5716aa4648bd2d9e6ba29a91af48afa719
pkaur376 <90533864+pkaur376@users.noreply.github.com>
2022-04-12 17:40:55 -0400

    Add files via upload

---
+++ source.js
@@ -0,0 +1,12 @@
+
+function display(){
+    document.getElementById('f1').innerHTML = document.myform;
+
+}
+
+function hiddenBox(){
+    input = document.createElement("input");
+    input.setAttribute("type", "text");
+    document.body.appendChild(input);
+} 
+
